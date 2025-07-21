b59f9145f6130212340489c933466441f7d8cf11
sudharsan <sudharsans.developer@gmail.com>
2025-07-21 12:22:13 +0530

    update company form field module

--- src/routes/v1/admin/companyFormFieldRoutes.js
+++ src/routes/v1/admin/companyFormFieldRoutes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const CompanyFormFieldRepository = require('../../../repositories/CompanyFormFieldRepository');
+const FormFieldRepository = require('../../../repositories/FromFieldRepository');
 const CompanyFormFieldService = require('../../../services/CompanyFormFieldService');
 const CompanyFormFieldController = require('../../../controllers/CompanyFormFieldController');
 const { validate, createCompanyFormFieldSchema, validateParams, getByPkSchema, updateCompanyFormFieldSchema } = require('../../../validators/companyFormFieldValidation');
 
 const companyFormFieldRepository = new CompanyFormFieldRepository();
-const companyFormFieldService = new CompanyFormFieldService(companyFormFieldRepository);
+const formFieldRepository = new FormFieldRepository();
+
+const companyFormFieldService = new CompanyFormFieldService(companyFormFieldRepository, formFieldRepository);
 const companyFormFieldController = new CompanyFormFieldController(companyFormFieldService);
 
 router.get('/', companyFormFieldController.getAll);
