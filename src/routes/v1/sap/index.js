197a329fa95306d04b73e36630d3321259e51070
sudharsan <sudharsans.developer@gmail.com>
2025-10-21 17:07:15 +0530

    update service api

--- src/routes/v1/sap/index.js
+++ src/routes/v1/sap/index.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const BPRoutes = require('./businesspartnerRoutes');
 const OrderRoutes = require('./orderRoutes');
 const ItemRoutes = require('./itemRoutes');
+const ServiceRoutes = require('./serviceRoutes');
 const PurchaseOrderRoutes = require('./purchaseOrderRoutes');
 
 router.use('/business-partners', BPRoutes);
 router.use('/orders', OrderRoutes);
 router.use('/purchase-orders', PurchaseOrderRoutes);
 router.use('/items', ItemRoutes);
+router.use('/services', ServiceRoutes);
 
 module.exports = router;
---
+++ src/routes/v1/sap/serviceRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const SAPController = require('../../../controllers/SAPController');
+
+router.get('/', SAPController.getServices);
+
+module.exports = router;
