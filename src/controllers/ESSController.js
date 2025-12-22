3dc750fb948a320fc46be701713d253316788e12
sudharsan <sudharsans.developer@gmail.com>
2025-12-22 19:31:00 +0400

    update the approval flow

--- src/controllers/ESSController.js
+++ src/controllers/ESSController.js
@@ -49,7 +49,7 @@ const getProjects = async (req, res) => {
 
 const getEmployes = async (req, res) => {
   try {
-    const response = await sapGetRequest(req, "/EmployeesInfo?$select=EmployeeID,ExternalEmployeeNumber,LastName,FirstName,eMail,MobilePhone,Department,WorkStreet,WorkZipCode");
+    const response = await sapGetRequest(req, "/EmployeesInfo?$select=EmployeeID,ExternalEmployeeNumber,LastName,FirstName,eMail,MobilePhone,Department,WorkStreet,WorkZipCode,Position");
     res.status(200).json(response.data);
   } catch (err) {
     console.error('SAP error:', err.message);
@@ -63,7 +63,7 @@ const getEmployeeProfile = async (req, res) => {
     if(!employeeId){
       res.status(404).json({message: "Employee Id not found!"});
     }
-    const response = await sapGetRequest(req, `/EmployeesInfo(${employeeId})?$select=EmployeeID,ExternalEmployeeNumber,JobTitle, LastName,FirstName,eMail,MobilePhone,Department, PassportNumber, Picture, WorkStreet,WorkZipCode`);
+    const response = await sapGetRequest(req, `/EmployeesInfo(${employeeId})?$select=EmployeeID,ExternalEmployeeNumber,JobTitle, LastName,FirstName,eMail,MobilePhone,Department, PassportNumber, Picture, WorkStreet,WorkZipCode,Position`);
     res.status(200).json(response.data);
   } catch (err) {
     console.error('SAP error:', err.message);
@@ -125,7 +125,7 @@ const findMissedCheckOuts = async (req, EmpId) => {
 
 const syncEmployees = async (req, res) => {
   try {
-    const employees = await sapGetRequest(req, "/EmployeesInfo?$select=EmployeeID,ExternalEmployeeNumber,LastName,FirstName,eMail,MobilePhone,Department,WorkStreet,WorkZipCode");
+    const employees = await sapGetRequest(req, "/EmployeesInfo?$select=EmployeeID,ExternalEmployeeNumber,LastName,FirstName,eMail,MobilePhone,Department,WorkStreet,WorkZipCode,Position");
     const userRepository = new UserRepository();
     const userService = new UserService(userRepository);
     const userController = new UserController(userService);
@@ -228,14 +228,35 @@ const createExpRequest = async (req, res) => {
   try {
     
     const user = req.user;
-    const emp = await sapGetRequest(req, `/EmployeesInfo(${user.EmployeeId})?$select=EmployeeID,ExternalEmployeeNumber,JobTitle, LastName,FirstName,eMail,MobilePhone,Department, PassportNumber, Picture, WorkStreet,WorkZipCode`);
+    const emp = await sapGetRequest(req, `/EmployeesInfo(${user.EmployeeId})?$select=EmployeeID,ExternalEmployeeNumber,JobTitle, LastName,FirstName,eMail,MobilePhone,Department, PassportNumber, Picture, WorkStreet,WorkZipCode, Position`);
+    console.log('emp', emp.data);
+    
+    const app_lev = await sapGetRequest(req, `/HLB_OAPP?$select=*&$filter=U_Cate eq '${emp.data.Position}' AND U_ESSApp eq 'Y' AND  U_HLB_EXP eq 'Y'`);
+console.log('app lev', app_lev.data);
+// console.log('app_lev.data.value[0].HLB_APP1Collection',app_lev.data.value[0].HLB_APP1Collection);
 
+  const isNeedApproval = app_lev.data.value?.[0]?.HLB_APP1Collection?.length ?? 0;;
+    
     let payload = req.body;
     
     payload.U_EmpID = user.EmployeeId || 0;
     payload.U_EmpName = emp.data.FirstName +" "+ emp.data.LastName || "";
+    payload.U_ApprSts = isNeedApproval ? "P":"A";
 
-    const response = await sapPostRequest(req, '/HLB_OECL', payload);         
+    const response = await sapPostRequest(req, '/HLB_OECL', payload);  
+    
+    let logPayload = {
+        "Name": payload.U_EmpName,
+        "U_DocType": "E",
+        "U_DocNo": response.data.DocNum,
+        "U_Stg": isNeedApproval?"1":"",
+        "U_AppId": isNeedApproval?app_lev.data.value?.[0]?.HLB_APP1Collection?.[0]?.U_ApprID:"",
+        "U_AppSts": isNeedApproval?"P":"A",
+        "U_ApprDt": null,
+        "U_ApprTm": null
+    }
+    await creatLogEntry(req, res, logPayload)
+  
     res.status(200).json({
         message: 'Expanse request submited successfully',
         data: response.data
@@ -249,8 +270,6 @@ const createExpRequest = async (req, res) => {
 const getAllExpList = async (req, res) => {
   try {
     const user = req.user;
-    console.log('user', user);
-    
     const response = await sapGetRequest(req, `/HLB_OECL?$orderby=DocEntry desc&$filter=U_EmpID eq '${user.EmployeeId}'`);
     res.status(200).json(response.data);
   } catch (err) {
@@ -259,4 +278,154 @@ const getAllExpList = async (req, res) => {
   }
 }
 
-module.exports = { getHolidays, getProjects, getEmployes, employeeCheckIn, employeeCheckOut, syncEmployees, getEmployeeProfile, isCheckedIn, missedOutNotification, getAllExpType, createExpRequest, getAllExpList }
+const getExp = async (req, res, id) => {
+  try {
+    const response = await sapGetRequest(req, `/HLB_OECL?$filter=DocNum eq ${id}`);
+    return response.data;
+  } catch (err) {
+    console.error('SAP error:', err.message);
+    res.status(500).json({ message: 'Error fetching Expanse Type', error: err.message });
+  }
+}
+
+const getAllLogsList = async (req, res) => {
+  try {
+    const user = req.user;
+    const response = await sapGetRequest(req, `/U_HLB_OAPL`);
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.error('SAP error:', err.message);
+    res.status(500).json({ message: 'Error fetching Logs', error: err.message });
+  }
+}
+
+const creatLogEntry = async (req, res, payload) => {
+  try{
+    const response = await sapPostRequest(req, `/U_HLB_OAPL`, payload)
+    return
+  } catch(err){
+    console.error('SAP error:', err.message);
+    res.status(500).json({ message: 'Error while creating Logs', error: err.message });
+  }
+}
+
+const getApprovalRequestsList = async (req, res) => {
+  try{
+    const user = req.user;
+    const response = await sapGetRequest(req, `/U_HLB_OAPL?$filter=U_AppId eq '${user.EmployeeId}'`);
+    let requests = [];
+
+    const promises = response.data.value.map(async (log) => {
+      const expRequest = await sapGetRequest(req, `/HLB_OECL(${log.U_DocNo})`);
+      const combinedData = {...log, RequstData:expRequest.data}
+      return combinedData;
+    });
+
+    requests = await Promise.all(promises);
+    res.status(200).json(requests);
+  } catch(err){
+    console.error('SAP error:', err.message);
+    res.status(500).json({ message: 'Error while getting requests', error: err.message });
+  }
+}
+
+const RequestResponse = async (req, res) => {
+  try{
+    const user = req.user;
+    const {id} = req.params;
+    const payload = req.body;
+
+    console.log('payload', payload);
+    
+
+    const checkStatus = await sapGetRequest(req, `/U_HLB_OAPL(${id})`);
+    
+    const expReq = await sapGetRequest(req, `/HLB_OECL(${checkStatus.data.U_DocNo})`);
+    console.log('checkstaus', checkStatus.data);
+    console.log('expreq', expReq.data);
+    
+    const requester = await sapGetRequest(req, `/EmployeesInfo(${expReq.data.U_EmpID})?$select=EmployeeID,ExternalEmployeeNumber,JobTitle, LastName,FirstName,eMail,MobilePhone,Department, PassportNumber, Picture, WorkStreet,WorkZipCode, Position`);
+    console.log('requester', requester.data);
+    
+    const app_lev = await sapGetRequest(req, `/HLB_OAPP?$select=*&$filter=U_Cate eq '${requester.data.Position}' AND U_ESSApp eq 'Y' AND  U_HLB_EXP eq 'Y'`);
+
+    console.log('app_lev', app_lev.data);
+    
+    console.log('app lev coll', app_lev.data.value?.[0]?.HLB_APP1Collection);
+
+    const totalAprLevs = app_lev.data.value?.[0]?.HLB_APP1Collection.length;
+    
+    const getLogs = await sapGetRequest(req, `/U_HLB_OAPL?$filter=U_DocNo eq '${checkStatus.data.U_DocNo}'`);
+
+    console.log('checkStatus', checkStatus.data);
+    console.log('getLogs', getLogs.data.value.length);
+    const totalLogs = getLogs.data.value.length;
+    
+    if(user.EmployeeId !== checkStatus.data.U_AppId){
+      return res.status(403).json({message: "You don't have permission to approve this request!"});
+    }
+
+    if(checkStatus.data.U_AppSts === "A"){
+      return res.status(200).json({message: "This request is already approved!"});
+    }
+
+    if(checkStatus.data.U_AppSts === "R"){
+      return res.status(200).json({message: "This request is already Rejected!"});
+    }
+
+    const patchReq = await sapPatchRequest(req, `/U_HLB_OAPL(${id})`, payload);
+    const updatedData = await sapGetRequest(req, `/U_HLB_OAPL(${id})`);
+    console.log('updated data', updatedData.data);
+    // console.log('data', app_lev.data.value[0].HLB_APP1Collection[getLogs.data.value.length].U_ApprID);
+
+    if(updatedData.data.U_AppSts == "R"){
+      console.log('inside reject');
+      
+        const empReqPayload = {
+            "U_ApprSts":"R"
+        }
+        await sapPatchRequest(req, `/HLB_OECL(${updatedData.data.U_DocNo})`, empReqPayload); 
+        return res.status(200).json({message: "Response submitted successfully!"})   
+    }
+
+    if(totalAprLevs == totalLogs){
+      console.log('inside reject');
+
+      const pending = getLogs.data.value.filter(val => val.U_AppSts === "R" || val.U_AppSts === "P");
+      if(pending.length > 0){
+         const empReqPayload = {
+            "U_ApprSts":"A"
+        }
+        await sapPatchRequest(req, `/HLB_OECL(${updatedData.data.U_DocNo})`, empReqPayload);
+        return res.status(200).json({message: "Response submitted successfully!"})
+      }
+    }
+    console.log('status', updatedData.data.U_AppSts);
+    console.log('status val', totalAprLevs > totalLogs);
+    console.log('condition', totalAprLevs, totalLogs);
+    console.log('if condition', updatedData.data.U_AppSts == "A" && totalAprLevs > totalLogs);
+    
+    if(updatedData.data.U_AppSts == "A" && totalAprLevs > totalLogs){
+      console.log('entry');
+        let logPayload = {
+          "Name": updatedData.data.Name,
+          "U_DocType": "E",
+          "U_DocNo": updatedData.data.U_DocNo,
+          "U_Stg": app_lev.data.value?.[0]?.HLB_APP1Collection?.[totalLogs].U_Stg,
+          "U_AppId": app_lev.data.value?.[0]?.HLB_APP1Collection?.[totalLogs].U_ApprID,
+          "U_AppSts": "P",
+          "U_ApprDt": null,
+          "U_ApprTm": null
+      }
+      console.log('logpayload', logPayload);
+      
+      await creatLogEntry(req, res, logPayload)
+    }
+    res.status(204).json({message:"Response submited!"});
+  } catch(err){
+    console.error('SAP error:', err.message);
+    res.status(500).json({ message: 'Error while updating request', error: err.message });
+  }
+}
+
+module.exports = { getHolidays, getProjects, getEmployes, employeeCheckIn, employeeCheckOut, syncEmployees, getEmployeeProfile, isCheckedIn, missedOutNotification, getAllExpType, getExp, createExpRequest, getAllExpList, getAllLogsList, getApprovalRequestsList, RequestResponse }
--- src/routes/v1/ess/index.js
+++ src/routes/v1/ess/index.js
@@ -6,11 +6,13 @@ const ProjectRoutes = require('./projectRoutes');
 const EmployeeRoutes = require('./employeeRoutes');
 const AttendanceRoutes = require('./attendanceRoutes');
 const ExpanseRoutes = require('./expanseRoutes');
+const ApprovalRoutes = require('./approvalLogRoutes');
 
 router.use('/holidays', HolidayRoutes);
 router.use('/projects', ProjectRoutes);
 router.use('/employees', EmployeeRoutes);
 router.use('/attendance', AttendanceRoutes);
 router.use('/expanses', ExpanseRoutes);
+router.use('/approvals', ApprovalRoutes);
 
 module.exports = router;
