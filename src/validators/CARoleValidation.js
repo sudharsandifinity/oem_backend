fb4ff210562edb6358652309d641318a22cd8973
sudharsan <sudharsans.developer@gmail.com>
2026-05-19 05:44:45 -0700

    role validator for ca

---
+++ src/validators/CARoleValidation.js
@@ -0,0 +1,109 @@
+const Joi = require('joi');
+
+const createRoleSchema = Joi.object({
+  name: Joi.string().required().messages({
+    'string.empty': 'Name is required',
+    'any.required': 'Name is required'
+  }),
+
+  // scope: Joi.string().valid('master', 'user').required().messages({
+  //   'any.only': 'Scope must be either "master" or "user"',
+  //   'string.empty': 'Scope is required',
+  //   'any.required': 'Scope is required'
+  // }),
+
+//   branchId: Joi.string().allow('').optional().messages({
+//     'string.base': 'Branch ID must be a string'
+// }),
+
+  companyId: Joi.string().required().messages({
+    'string.base': 'Company ID must be a string',
+    'any.required': 'Company ID is required'
+  }),
+
+
+  status: Joi.number().max(1).optional().messages({
+    'number.base': 'Status must be a number',
+    'number.max': 'Status cannot be more than 1'
+  }),
+
+  userMenuIds: Joi.array().items(
+    Joi.object({
+      menuId: Joi.string().required(),
+      can_list_view: Joi.boolean().optional(),
+      can_create: Joi.boolean().optional(),
+      can_edit: Joi.boolean().optional(),
+      can_view: Joi.boolean().optional(),
+      can_delete: Joi.boolean().optional()
+    })
+  ).optional()
+});
+
+const updateRoleSchema = Joi.object({
+  name: Joi.string().optional().messages({
+    'string.empty': 'Name cannot be empty'
+  }),
+
+  companyId: Joi.string().optional().messages({
+    'string.base': 'Company ID must be a string'
+  }),
+
+  status: Joi.number().max(1).optional().messages({
+    'number.base': 'Status must be a number',
+    'number.max': 'Status cannot be more than 1'
+  }),
+
+  userMenuIds: Joi.array()
+    .items(
+      Joi.object({
+        menuId: Joi.string().required().messages({
+          'any.required': 'Menu ID is required'
+        }),
+        can_list_view: Joi.boolean().optional(),
+        can_create: Joi.boolean().optional(),
+        can_edit: Joi.boolean().optional(),
+        can_view: Joi.boolean().optional(),
+        can_delete: Joi.boolean().optional()
+      })
+    )
+    .optional()
+    .messages({
+      'array.base': 'User menus must be an array of objects'
+    })
+});
+
+const getByPkSchema = Joi.object({
+  id: Joi.string().required().messages({
+    'any.required': 'ID is required',
+    'string.base': 'ID must be a string'
+  })
+});
+
+
+function validate(schema) {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: true });
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    next();
+  };
+}
+
+function validateParams(schema) {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.params, { abortEarly: true });
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    next();
+  };
+}
+
+module.exports = {
+  createRoleSchema,
+  updateRoleSchema,
+  getByPkSchema,
+  validate,
+  validateParams
+};
